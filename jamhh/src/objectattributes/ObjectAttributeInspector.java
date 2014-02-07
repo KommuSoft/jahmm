9ebed974c7b5f99248427734921debbaac73aac1
KommuSoft <vanonsem.willem@gmail.com>
2014-02-07 16:56:30 +0100

    core of inspector

--- jamhh/src/objectattributes/ObjectAttributeInspector.java
+++ jamhh/src/objectattributes/ObjectAttributeInspector.java
@@ -1,9 +1,25 @@
 package objectattributes;
 
+import java.lang.reflect.Method;
+import java.util.LinkedList;
+
 /**
  *
  * @author kommusoft
  */
 public class ObjectAttributeInspector {
+    
+    private ObjectAttributeInspector () {}
+    
+    public static Iterable<ObjectAttribute> inspect (Class toinspect) {
+        LinkedList<ObjectAttribute> ll = new LinkedList<>();
+        for(Method method : toinspect.getMethods()) {
+            ObjectAttributeAnnotation oaa = method.getAnnotation(ObjectAttributeAnnotation.class);
+            if(oaa != null) {
+                ll.add(null);
+            }
+        }
+        return ll;
+    }
     
 }
