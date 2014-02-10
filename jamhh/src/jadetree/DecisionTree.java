a661f0aad744ac68917d654acb515819cd981fdf
KommuSoft <vanonsem.willem@gmail.com>
2014-02-10 20:02:37 +0100

    recursive adding

--- jamhh/src/jadetree/DecisionTree.java
+++ jamhh/src/jadetree/DecisionTree.java
@@ -9,38 +9,39 @@ import objectattributes.ObjectAttribute;
  * @param <TSource> The source of the types to classify.
  */
 public class DecisionTree<TSource> {
-    
-    private final ArrayList<ObjectAttribute<? super TSource,?>> sourceattributes = new ArrayList<>();
-    private final ArrayList<ObjectAttribute<? super TSource,?>> targetattributes = new ArrayList<>();
+
+    private final ArrayList<ObjectAttribute<? super TSource, ?>> sourceattributes = new ArrayList<>();
+    private final ArrayList<ObjectAttribute<? super TSource, ?>> targetattributes = new ArrayList<>();
     private DecisionNode root = new DecisionLeaf();
-    
+
+    public void insert(TSource element) {
+        this.root.insert(element);
+    }
+
     public class DecisionTreeNode<TTarget> {
-        
-        ObjectAttribute<? super TSource,TTarget> decisionattribute;
-        
+
+        ObjectAttribute<? super TSource, TTarget> decisionattribute;
+
     }
-    
-    public void insert (TSource element) {
-    }
-    
+
     private abstract class DecisionNode {
-        
-        public abstract boolean isLeaf ();
-        
-        public abstract DecisionNode nextHop (TSource source);
-        
-        public abstract double expandScore ();
-        
-        public abstract DecisionNode expand ();
-        
-        public void insert () {
-            
+
+        public abstract boolean isLeaf();
+
+        public abstract DecisionNode nextHop(TSource source);
+
+        public abstract double expandScore();
+
+        public abstract DecisionNode expand();
+
+        public void insert(TSource source) {
+            this.nextHop(source).insert(source);
         }
-        
+
     }
-    
+
     private class DecisionLeaf extends DecisionNode {
-        
+
         private final ArrayList<TSource> memory = new ArrayList<>();
 
         @Override
@@ -62,7 +63,12 @@ public class DecisionTree<TSource> {
         public DecisionNode expand() {
             throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         }
-        
+
+        @Override
+        public void insert(TSource source) {
+            memory.add(source);
+        }
+
     }
-    
+
 }
