b488ef6d1810042f35e3d4ca97052c9c735e735b
KommuSoft <vanonsem.willem@gmail.com>
2014-02-10 21:36:07 +0100

    interfacing

--- jamhh/src/objectattributes/ObjectAttribute.java
+++ jamhh/src/objectattributes/ObjectAttribute.java
@@ -1,6 +1,7 @@
 package objectattributes;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.List;
 import utils.Name;
 
 /**
@@ -14,5 +15,9 @@ public interface ObjectAttribute<TSource, TTarget> extends Name {
     TTarget getAttribute(TSource source) throws IllegalAccessException, InvocationTargetException;
 
     ObjectAttributeType getType();
+    
+    double calculateScore (List<TSource> source);
+    
+    void createDecisionNode (List<TSource> source);
 
 }
