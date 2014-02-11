d4d37fea13180a91ea3548c4f0c46f6e0a67d60a
KommuSoft <vanonsem.willem@gmail.com>
2014-02-11 23:29:36 +0100

    factories arrayblocking

--- jamhh/src/utils/CollectionFactoryMethods.java
+++ jamhh/src/utils/CollectionFactoryMethods.java
@@ -13,6 +13,44 @@ public class CollectionFactoryMethods {
     private CollectionFactoryMethods() {
     }
     
+    public static <T> FactoryMethod<ArrayBlockingQueue<T>> arrayBlockingQueueFactory(int capacity) {
+        return new FactoryMethod<ArrayBlockingQueue<T>>() {
+            
+            private int capacity;
+            
+            @Override
+            public ArrayBlockingQueue<T> generate() {
+                return new ArrayBlockingQueue<T>(capacity, fair, c);
+            }
+            
+            private FactoryMethod<ArrayBlockingQueue<T>> initialize(int capacity) {
+                this.capacity = capacity;
+                return this;
+            }
+            
+        }.initialize(capacity);
+    }
+    
+    public static <T> FactoryMethod<ArrayBlockingQueue<T>> arrayBlockingQueueFactory(int capacity, boolean fair) {
+        return new FactoryMethod<ArrayBlockingQueue<T>>() {
+            
+            private int capacity;
+            private boolean fair;
+            
+            @Override
+            public ArrayBlockingQueue<T> generate() {
+                return new ArrayBlockingQueue<T>(capacity, fair, c);
+            }
+            
+            private FactoryMethod<ArrayBlockingQueue<T>> initialize(int capacity, boolean fair) {
+                this.capacity = capacity;
+                this.fair = fair;
+                return this;
+            }
+            
+        }.initialize(capacity, fair);
+    }
+    
     public static <T> FactoryMethod<ArrayBlockingQueue<T>> arrayBlockingQueueFactory(int capacity, boolean fair, Collection<? extends T> c) {
         return new FactoryMethod<ArrayBlockingQueue<T>>() {
             
@@ -34,6 +72,7 @@ public class CollectionFactoryMethods {
             
         }.initialize(capacity, fair, c);
     }
+    
     
     public static <T> FactoryMethod<ArrayList<T>> arrayListFactory() {
         return new FactoryMethod<ArrayList<T>>() {
