47f028bc26263ad02a1d0942863d1e76fb1214f5
KommuSoft <vanonsem.willem@gmail.com>
2014-02-11 23:19:20 +0100

    start collectionfactories

---
+++ jamhh/src/utils/CollectionFactoryMethods.java
@@ -0,0 +1,49 @@
+package utils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.ArrayBlockingQueue;
+
+/**
+ *
+ * @author kommusoft
+ */
+public class CollectionFactoryMethods {
+    
+    private CollectionFactoryMethods() {
+    }
+    
+    public static <T> FactoryMethod<ArrayBlockingQueue<T>> arrayBlockingQueueFactory(int capacity, boolean fair, Collection<? extends T> c) {
+        return new FactoryMethod<ArrayBlockingQueue<T>>() {
+            
+            private int capacity;
+            private boolean fair;
+            private Collection<? extends T> c;
+            
+            @Override
+            public ArrayBlockingQueue<T> generate() {
+                return new ArrayBlockingQueue<T>(capacity, fair, c);
+            }
+            
+            private FactoryMethod<ArrayBlockingQueue<T>> initialize(int capacity, boolean fair, Collection<? extends T> c) {
+                this.capacity = capacity;
+                this.fair = fair;
+                this.c = c;
+                return this;
+            }
+            
+        }.initialize(capacity, fair, c);
+    }
+    
+    public static <T> FactoryMethod<ArrayList<T>> arrayListFactory() {
+        return new FactoryMethod<ArrayList<T>>() {
+            
+            @Override
+            public ArrayList<T> generate() {
+                return new ArrayList<T>();
+            }
+            
+        };
+    }
+    
+}
