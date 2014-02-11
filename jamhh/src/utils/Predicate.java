168a768d8a81ef94b96afea0ea9dd2ca73b1e814
KommuSoft <vanonsem.willem@gmail.com>
2014-02-12 00:37:12 +0100

    predicate

---
+++ jamhh/src/utils/Predicate.java
@@ -0,0 +1,10 @@
+package utils;
+
+/**
+ *
+ * @author kommusoft
+ * @param <TSource> The domain of the predicate.
+ */
+public interface Predicate<TSource> extends Function<TSource, Boolean> {
+
+}
