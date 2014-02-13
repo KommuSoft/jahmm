09849145bd07bff8101d25879dcc511a07d8b204
KommuSoft <vanonsem.willem@gmail.com>
2014-02-13 22:44:36 +0100

    introduce concept of raw entropy

--- jamhh/src/jadetree/DecisionTreeUtils.java
+++ jamhh/src/jadetree/DecisionTreeUtils.java
@@ -13,7 +13,7 @@ import jutlis.tuples.Tuple2;
  */
 public class DecisionTreeUtils {
 
-    public static <TSource, TTarget> double calculateEntropy(Iterable<? extends TSource> sources, final HashMap<TTarget, Integer> frequency, Function<TSource, TTarget> function, Holder<Integer> total) {
+    public static <TSource, TTarget> double calculateRawEntropy(Iterable<? extends TSource> sources, final HashMap<TTarget, Integer> frequency, Function<TSource, TTarget> function, Holder<Integer> total) {
         int ttl = 0;
         for (TSource s : sources) {
             TTarget target = function.evaluate(s);
@@ -24,31 +24,40 @@ public class DecisionTreeUtils {
         for (Integer fi : frequency.values()) {
             entropy -= fi * Math.log(fi);
         }
-        entropy = (entropy / ttl + Math.log(ttl)) / Math.log(2.0d);
         if (total != null) {
             total.setData(ttl);
         }
         return entropy;
     }
 
-    /*public static <TSource, TTarget> int calculateEntropyFlipIndex(Iterable<? extends TSource> sources, final HashMap<TTarget, Integer> frequency, Function<TSource, TTarget> function, Tuple2<Integer, Double> total_entropy) {
-     final double log2 = 1.0 / Math.log(2.0);
-     int ttl = 0;
-     for (TSource s : sources) {
-     TTarget target = function.evaluate(s);
-     CollectionUtils.incrementKey(frequency, target);
-     ttl++;
-     }
-     double entropy = 0.0;
-     for (Integer fi : frequency.values()) {
-     double p = (double) fi / ttl;
-     entropy -= p * log2 * Math.log(p);
-     }
-     if (total != null) {
-     total.setData(ttl);
-     }
-     return entropy;
-     }*/
+    public static <TSource, TTarget> double calculateEntropy(Iterable<? extends TSource> sources, final HashMap<TTarget, Integer> frequency, Function<TSource, TTarget> function, Holder<Integer> total) {
+        Holder<Integer> subholder = total;
+        if (subholder == null) {
+            subholder = new HolderBase<>();
+        }
+        double entropy = calculateRawEntropy(sources, frequency, function, total);
+        int ttl = subholder.getData();
+        return (entropy / ttl + Math.log(ttl)) / Math.log(2.0d);
+    }
+
+    public static <TSource, TTarget> int calculateEntropyFlipIndex(Iterable<? extends TSource> sources, Function<TSource, TTarget> function, Tuple2<Integer, Double> total_entropy) {
+        final HashMap<TTarget, Integer> frequency = new HashMap<>();
+        int ttl = 0x00, maxFlip = -0x01;
+        double maxEntropy = 0.0d, rawEntropy = 0.0d;
+        for (TSource s : sources) {
+            TTarget target = function.evaluate(s);
+            CollectionUtils.incrementKey(frequency, target);
+            ttl++;
+            //TODO: recalculate entropy
+            if (rawEntropy > maxEntropy) {
+                maxEntropy = rawEntropy;
+                maxFlip = ttl - 0x01;
+            }
+
+        }
+        return maxFlip;
+    }
+
     public static <TSource, TTarget> double calculateEntropyPartition(Iterable<? extends Iterable<? extends TSource>> sources, Function<TSource, TTarget> function) {
         final HashMap<TTarget, Integer> frequency = new HashMap<>();
         Holder<Integer> ttl = new HolderBase<>();
