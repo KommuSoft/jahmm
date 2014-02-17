3922e1f22bee48177b153b6aeae69fafe2f1c414
KommuSoft <vanonsem.willem@gmail.com>
2014-02-17 13:02:55 +0100

    adding entropy test

---
+++ jamhh/test/jadetree/DecisionTreeUtilsTest.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jadetree;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author kommusoft
+ */
+public class DecisionTreeUtilsTest {
+    
+    public DecisionTreeUtilsTest() {
+    }
+
+    @Test
+    public void testCalculateRawEntropy() {
+        System.out.println("calculateRawEntropy");
+        double expResult = 0.0;
+        /*double result = DecisionTreeUtils.calculateRawEntropy(null);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");//*/
+    }
+
+    @Test
+    public void testCalculateEntropy_4args() {
+        System.out.println("calculateEntropy");
+        double expResult = 0.0;
+        /*double result = DecisionTreeUtils.calculateEntropy(null);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");//*/
+    }
+
+    @Test
+    public void testCalculateEntropy_Iterable_Function() {
+        System.out.println("calculateEntropy");
+        double expResult = 0.0;
+        /*double result = DecisionTreeUtils.calculateEntropy(null);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");//*/
+    }
+
+    @Test
+    public void testCalculateEntropyFlipIndex() {
+        System.out.println("calculateEntropyFlipIndex");
+        int expResult = 0;
+        /*int result = DecisionTreeUtils.calculateEntropyFlipIndex(null);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");//*/
+    }
+
+    @Test
+    public void testCalculateEntropyPartition() {
+        System.out.println("calculateEntropyPartition");
+        double expResult = 0.0;
+        /*double result = DecisionTreeUtils.calculateEntropyPartition(null);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");//*/
+    }
+    
+}
