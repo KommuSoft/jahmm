6616a6710d92ea410cf4313a072f4c501e77784c
KommuSoft <vanonsem.willem@gmail.com>
2014-02-18 01:49:19 +0100

    linking to the composite design pattern

--- jamhh/src/jahmm/jadetree/DecisionNode.java
+++ jamhh/src/jahmm/jadetree/DecisionNode.java
@@ -1,11 +1,13 @@
 package jahmm.jadetree;
 
+import jutils.designpatterns.CompositeComponent;
+
 /**
  *
  * @author kommusoft
  * @param <TSource> The type of objects classified and stored in the tree.
  */
-public interface DecisionNode<TSource> {
+public interface DecisionNode<TSource> extends CompositeComponent<DecisionNode<TSource>> {
 
     /**
      * @return the tree
