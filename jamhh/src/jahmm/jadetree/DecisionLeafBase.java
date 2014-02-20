df1b2e0373b2624e5e3382d8c97d925994595a93
KommuSoft <vanonsem.willem@gmail.com>
2014-02-20 17:37:32 +0100

    simplified decisionleaf

--- jamhh/src/jahmm/jadetree/DecisionLeafBase.java
+++ jamhh/src/jahmm/jadetree/DecisionLeafBase.java
@@ -3,9 +3,13 @@ package jahmm.jadetree;
 import jahmm.jadetree.abstracts.DecisionInode;
 import jahmm.jadetree.abstracts.DecisionLeaf;
 import jahmm.jadetree.abstracts.DecisionRealInode;
+import jahmm.jadetree.abstracts.DecisionRealNode;
+import jahmm.jadetree.abstracts.DecisionTree;
+import jahmm.jadetree.objectattributes.ObjectAttribute;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
 import jutils.iterators.SingleIterable;
 import jutlis.tuples.HolderBase;
 
@@ -16,9 +20,11 @@ import jutlis.tuples.HolderBase;
  */
 public final class DecisionLeafBase<TSource> extends DecisionRealNodeBase<TSource> implements DecisionLeaf<TSource> {
 
+    private static final Logger LOG = Logger.getLogger(DecisionLeafBase.class.getName());
+
     private final List<TSource> memory;
     private double expandScore = Double.NaN;
-    private int splitIndex = 0x00;
+    private int splitIndex = -0x01;
     private final HolderBase<Object> splitData = new HolderBase<>();
 
     public DecisionLeafBase(final DecisionInode<TSource> parent) {
@@ -35,6 +41,55 @@ public final class DecisionLeafBase<TSource> extends DecisionRealNodeBase<TSourc
         for (TSource item : memory) {
             this.memory.add(item);
         }
+    }
+
+    @Override
+    public double expandScore() {
+        if (Double.isNaN(this.expandScore)) {
+            this.expandScore = this.recalculateExpandScore();
+        }
+        return this.expandScore;
+    }
+
+    @Override
+    public void insert(TSource source) {
+        this.memory.add(source);
+    }
+
+    @Override
+    public boolean isLeaf() {
+        return true;
+    }
+
+    @Override
+    public boolean isDirty() {
+        return Double.isNaN(this.expandScore) || super.isDirty();
+    }
+
+    @Override
+    public void makeDirty() {
+        this.expandScore = Double.NaN;
+        this.splitData.setData(null);
+        this.splitIndex = -0x01;
+        super.makeDirty();
+    }
+
+    private double recalculateExpandScore() {
+        double maxScore = Double.NEGATIVE_INFINITY;
+        int maxIndex = 0x00, i = 0x00;
+        HolderBase<Object> curData = new HolderBase<>();
+        ObjectAttribute<TSource, Object> target = this.getTree().getTargetAttribute();
+        for (ObjectAttribute<TSource, ?> oa : this.getTree().getSourceAttributes()) {
+            double sc = oa.calculateScore(this.memory, target, curData);
+            if (sc > maxScore) {
+                maxScore = sc;
+                maxIndex = i;
+                this.splitData.copyFrom(curData);
+            }
+            i++;
+        }
+        this.splitIndex = maxIndex;
+        return maxScore;
     }
 
     @Override
@@ -59,7 +114,6 @@ public final class DecisionLeafBase<TSource> extends DecisionRealNodeBase<TSourc
 
     @Override
     public void expandThis() {
-        this.expandScore();
         DecisionTree<TSource> tree = this.getTree();
         this.expandScore();
         DecisionRealNode<TSource> foo = tree.getSourceAttributes().get(splitIndex).createDecisionNode(this.getParent(), this.memory, tree.getTargetAttribute(), this.splitData);
