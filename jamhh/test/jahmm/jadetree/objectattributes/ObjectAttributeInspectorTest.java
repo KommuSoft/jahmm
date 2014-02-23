e37150244166f998def089993aca196cb39702a5
KommuSoft <vanonsem.willem@gmail.com>
2014-02-24 00:35:21 +0100

    test organisation fix

--- jamhh/test/jahmm/jadetree/objectattributes/ObjectAttributeInspectorTest.java
+++ jamhh/test/jahmm/jadetree/objectattributes/ObjectAttributeInspectorTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package objectattributes;
+package jahmm.jadetree.objectattributes;
 
 import jahmm.jadetree.objectattributes.ObjectAttributeAnnotation;
 import jahmm.jadetree.objectattributes.ObjectAttribute;
