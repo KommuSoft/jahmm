1e4382db47045d5205ddd73845f24a18f52491ea
KommuSoft <vanonsem.willem@gmail.com>
2014-03-06 22:57:24 +0100

    noa tests finished

--- jahmm/test/jahmm/jadetree/foo/TestLeapYear.java
+++ jahmm/test/jahmm/jadetree/foo/TestLeapYear.java
@@ -17,49 +17,54 @@ public class TestLeapYear {
         this.year = year;
     }
 
+    @ObjectAttributeAnnotation(name = "year")
+    public int year() {
+        return year;
+    }
+
     @ObjectAttributeAnnotation(name = "div2")
-    public boolean Div2() {
+    public boolean div2() {
         return (year % 2) == 0;
     }
 
     @ObjectAttributeAnnotation(name = "div4")
-    public boolean Div4() {
+    public boolean div4() {
         return (year % 4) == 0;
     }
 
     @ObjectAttributeAnnotation(name = "div8")
-    public boolean Div8() {
+    public boolean div8() {
         return (year % 8) == 0;
     }
 
     @ObjectAttributeAnnotation(name = "div16")
-    public boolean Div16() {
+    public boolean div16() {
         return (year % 16) == 0;
     }
 
     @ObjectAttributeAnnotation(name = "div100")
-    public boolean Div100() {
+    public boolean div100() {
         return (year % 100) == 0;
     }
 
     @ObjectAttributeAnnotation(name = "div200")
-    public boolean Div200() {
+    public boolean div200() {
         return (year % 200) == 0;
     }
 
     @ObjectAttributeAnnotation(name = "div500")
-    public boolean Div500() {
+    public boolean div500() {
         return (year % 500) == 0;
     }
 
     @ObjectAttributeAnnotation(name = "div1000")
-    public boolean Div1000() {
+    public boolean div1000() {
         return (year % 1000) == 0;
     }
 
     @ObjectAttributeAnnotation(name = "leap")
     public boolean isLeapYear() {
-        return this.Div4() && (this.Div1000() || !this.Div100());
+        return this.div4() && (this.div1000() || !this.div100());
     }
 
 }
--- jahmm/test/jahmm/jadetree/objectattributes/NominalInspectedObjectAttributeTest.java
+++ jahmm/test/jahmm/jadetree/objectattributes/NominalInspectedObjectAttributeTest.java
@@ -15,6 +15,8 @@ import utils.TestParameters;
 public class NominalInspectedObjectAttributeTest {
 
     private static final Logger LOG = Logger.getLogger(NominalInspectedObjectAttributeTest.class.getName());
+    
+    String[] names = new String[] {"div2","div4","div8","div16","div100","div200","div500","div1000","leap"};
 
     public NominalInspectedObjectAttributeTest() {
     }
@@ -37,12 +39,70 @@ public class NominalInspectedObjectAttributeTest {
      */
     @Test
     public void testEvaluate() {
-        ObjectAttribute<TestLeapYear, ? extends Object> tly = ObjectAttributeInspector.inspect(TestLeapYear.class, "div4");
+        ObjectAttribute<TestLeapYear, ? extends Object> tly;
+        NominalInspectedObjectAttribute<TestLeapYear, ? extends Object> nioa;
+        tly = ObjectAttributeInspector.inspect(TestLeapYear.class, "div2");
         AssertExtensions.assertTypeof(NominalInspectedObjectAttribute.class, tly);
-        NominalInspectedObjectAttribute<TestLeapYear, ? extends Object> nioa = (NominalInspectedObjectAttribute<TestLeapYear, ? extends Object>) tly;
+        nioa = (NominalInspectedObjectAttribute<TestLeapYear, ? extends Object>) tly;
         for (int i = 0x00; i < TestParameters.NUMBER_OF_TESTS; i++) {
             TestLeapYear tlyi = new TestLeapYear(i);
-            Assert.assertEquals(tlyi.Div4(), nioa.evaluate(tlyi));
+            Assert.assertEquals(tlyi.div2(), nioa.evaluate(tlyi));
+        }
+        tly = ObjectAttributeInspector.inspect(TestLeapYear.class, "div4");
+        AssertExtensions.assertTypeof(NominalInspectedObjectAttribute.class, tly);
+        nioa = (NominalInspectedObjectAttribute<TestLeapYear, ? extends Object>) tly;
+        for (int i = 0x00; i < TestParameters.NUMBER_OF_TESTS; i++) {
+            TestLeapYear tlyi = new TestLeapYear(i);
+            Assert.assertEquals(tlyi.div4(), nioa.evaluate(tlyi));
+        }
+        tly = ObjectAttributeInspector.inspect(TestLeapYear.class, "div8");
+        AssertExtensions.assertTypeof(NominalInspectedObjectAttribute.class, tly);
+        nioa = (NominalInspectedObjectAttribute<TestLeapYear, ? extends Object>) tly;
+        for (int i = 0x00; i < TestParameters.NUMBER_OF_TESTS; i++) {
+            TestLeapYear tlyi = new TestLeapYear(i);
+            Assert.assertEquals(tlyi.div8(), nioa.evaluate(tlyi));
+        }
+        tly = ObjectAttributeInspector.inspect(TestLeapYear.class, "div16");
+        AssertExtensions.assertTypeof(NominalInspectedObjectAttribute.class, tly);
+        nioa = (NominalInspectedObjectAttribute<TestLeapYear, ? extends Object>) tly;
+        for (int i = 0x00; i < TestParameters.NUMBER_OF_TESTS; i++) {
+            TestLeapYear tlyi = new TestLeapYear(i);
+            Assert.assertEquals(tlyi.div16(), nioa.evaluate(tlyi));
+        }
+        tly = ObjectAttributeInspector.inspect(TestLeapYear.class, "div100");
+        AssertExtensions.assertTypeof(NominalInspectedObjectAttribute.class, tly);
+        nioa = (NominalInspectedObjectAttribute<TestLeapYear, ? extends Object>) tly;
+        for (int i = 0x00; i < TestParameters.NUMBER_OF_TESTS; i++) {
+            TestLeapYear tlyi = new TestLeapYear(i);
+            Assert.assertEquals(tlyi.div100(), nioa.evaluate(tlyi));
+        }
+        tly = ObjectAttributeInspector.inspect(TestLeapYear.class, "div200");
+        AssertExtensions.assertTypeof(NominalInspectedObjectAttribute.class, tly);
+        nioa = (NominalInspectedObjectAttribute<TestLeapYear, ? extends Object>) tly;
+        for (int i = 0x00; i < TestParameters.NUMBER_OF_TESTS; i++) {
+            TestLeapYear tlyi = new TestLeapYear(i);
+            Assert.assertEquals(tlyi.div200(), nioa.evaluate(tlyi));
+        }
+        tly = ObjectAttributeInspector.inspect(TestLeapYear.class, "div500");
+        AssertExtensions.assertTypeof(NominalInspectedObjectAttribute.class, tly);
+        nioa = (NominalInspectedObjectAttribute<TestLeapYear, ? extends Object>) tly;
+        for (int i = 0x00; i < TestParameters.NUMBER_OF_TESTS; i++) {
+            TestLeapYear tlyi = new TestLeapYear(i);
+            Assert.assertEquals(tlyi.div500(), nioa.evaluate(tlyi));
+        }
+        tly = ObjectAttributeInspector.inspect(TestLeapYear.class, "div1000");
+        AssertExtensions.assertTypeof(NominalInspectedObjectAttribute.class, tly);
+        nioa = (NominalInspectedObjectAttribute<TestLeapYear, ? extends Object>) tly;
+        for (int i = 0x00; i < TestParameters.NUMBER_OF_TESTS; i++) {
+            TestLeapYear tlyi = new TestLeapYear(i);
+            Assert.assertEquals(tlyi.div1000(), nioa.evaluate(tlyi));
+        }
+        tly = ObjectAttributeInspector.inspect(TestLeapYear.class, "leap");
+        AssertExtensions.assertTypeof(NominalInspectedObjectAttribute.class, tly);
+        nioa = (NominalInspectedObjectAttribute<TestLeapYear, ? extends Object>) tly;
+        for (int i = 0x00; i < TestParameters.NUMBER_OF_TESTS; i++) {
+            TestLeapYear tlyi = new TestLeapYear(i);
+            Assert.assertEquals(tlyi.isLeapYear(), nioa.evaluate(tlyi));
         }
     }
 
@@ -51,11 +111,13 @@ public class NominalInspectedObjectAttributeTest {
      */
     @Test
     public void testGetName() {
-        ObjectAttribute<TestLeapYear, ? extends Object> tly = ObjectAttributeInspector.inspect(TestLeapYear.class, "div4");
-        AssertExtensions.assertTypeof(NominalInspectedObjectAttribute.class, tly);
-        Assert.assertEquals("div4", tly.getName());
-        NominalInspectedObjectAttribute<TestLeapYear, ? extends Object> nioa = (NominalInspectedObjectAttribute<TestLeapYear, ? extends Object>) tly;
-        Assert.assertEquals("div4", nioa.getName());
+        for(String name : names) {
+            ObjectAttribute<TestLeapYear, ? extends Object> tly = ObjectAttributeInspector.inspect(TestLeapYear.class, name);
+            AssertExtensions.assertTypeof(NominalInspectedObjectAttribute.class, tly);
+            Assert.assertEquals(name, tly.getName());
+            NominalInspectedObjectAttribute<TestLeapYear, ? extends Object> nioa = (NominalInspectedObjectAttribute<TestLeapYear, ? extends Object>) tly;
+            Assert.assertEquals(name, nioa.getName());
+        }
     }
 
 }
