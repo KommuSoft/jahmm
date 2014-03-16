82cc126f559f37955005508cf80019a4936862b1
KommuSoft <vanonsem.willem@gmail.com>
2014-03-16 18:35:49 +0100

    more tests

--- jahmm/test/jahmm/jadetree/PredicateDecisionNodeTest.java
+++ jahmm/test/jahmm/jadetree/PredicateDecisionNodeTest.java
@@ -39,7 +39,24 @@ public class PredicateDecisionNodeTest {
      */
     @Test
     public void testTest() {
-        fail("The test case is a prototype.");
+        PredicateDecisionNode<Integer> pdn1 = new PredicateDecisionNode<>(null,pred1);
+        Assert.assertTrue(pdn1.test(0x02));
+        Assert.assertTrue(pdn1.test(-0x02));
+        Assert.assertTrue(pdn1.test(0x00));
+        Assert.assertFalse(pdn1.test(0x04));
+        Assert.assertTrue(pdn1.test(Integer.MIN_VALUE));
+        Assert.assertFalse(pdn1.test(0x05));
+        Assert.assertFalse(pdn1.test(0x07));
+        Assert.assertFalse(pdn1.test(Integer.MAX_VALUE));
+        PredicateDecisionNode<String> pdn2 = new PredicateDecisionNode<>(null,pred2);
+        Assert.assertFalse(pdn2.test(null));
+        Assert.assertTrue(pdn2.test("zzz"));
+        Assert.assertTrue(pdn2.test("kommusoft"));
+        Assert.assertFalse(pdn2.test("ape"));
+        Assert.assertTrue(pdn2.test("monkey business"));
+        Assert.assertFalse(pdn2.test("kommu"));
+        Assert.assertFalse(pdn2.test("kola"));
+        Assert.assertTrue(pdn2.test("kommusoftz"));
     }
 
     /**
@@ -47,7 +64,10 @@ public class PredicateDecisionNodeTest {
      */
     @Test
     public void testGetPredicate() {
-        fail("The test case is a prototype.");
+        PredicateDecisionNode<Integer> pdn1 = new PredicateDecisionNode<>(null,pred1);
+        Assert.assertEquals(pred1,pdn1.getPredicate());
+        PredicateDecisionNode<String> pdn2 = new PredicateDecisionNode<>(null,pred2);
+        Assert.assertEquals(pred2,pdn2.getPredicate());
     }
 
 }
