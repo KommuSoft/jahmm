d0eae31c3552cf941c99b17ecccdb71e8ba9be4e
KommuSoft <vanonsem.willem@gmail.com>
2014-03-16 20:48:49 +0100

    more testdata

--- jahmm/test/utils/TestData.java
+++ jahmm/test/utils/TestData.java
@@ -1,5 +1,7 @@
 package utils;
 
+import jahmm.jadetree.foo.Test2B1T;
+import jahmm.jadetree.foo.TrisEnum;
 import jutlis.lists.ListArray;
 
 /**
@@ -18,6 +20,10 @@ public class TestData {
     public static final ListArray<String> vals2le = new ListArray<>(new String[]{null, "ape", "kommu", "kola"});
     public static final String split2 = "kommusoft";
     public static final String name2 = "username";
+    public static final Test2B1T[] vals3 = new Test2B1T[]{new Test2B1T(false, false, TrisEnum.Odin), new Test2B1T(false, true, TrisEnum.Dva), new Test2B1T(true, false, TrisEnum.Tri), new Test2B1T(true, true, TrisEnum.Odin)};
+    public static final ListArray<Test2B1T> vals3a = new ListArray<>(new Test2B1T(false, false, TrisEnum.Odin), new Test2B1T(true, true, TrisEnum.Odin));
+    public static final ListArray<Test2B1T> vals3b = new ListArray<>(new Test2B1T(false, true, TrisEnum.Dva));
+    public static final ListArray<Test2B1T> vals3c = new ListArray<>(new Test2B1T(true, false, TrisEnum.Tri));
 
     private TestData() {
     }
